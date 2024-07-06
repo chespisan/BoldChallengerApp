06f230c48d2b6e109e6d9f7fe4cc9c2ac53e99d8
Sergio <sergio@MacBook-Pro.local>
2024-07-06 14:54:35 -0700

    feat: update header component and added table, checkboxgroup components

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,2 +1,4 @@
 export { HeaderComponent } from "app/presentation/components/header";
 export { TabsComponent } from "app/presentation/components/tabs";
+export { TableComponent } from "app/presentation/components/table";
+export { CheckboxComponent } from "app/presentation/components/checkbox";
